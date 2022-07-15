886661219af3763d16e302ec888b5a35eda3e85a
Marcos Carvalho <mp.doublebass@gmail.com>
2022-07-14 21:52:00 -0300

    firt gem commit

---
+++ spec/mini/rails_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Mini::Rails do
+  it "has a version number" do
+    expect(Mini::Rails::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
